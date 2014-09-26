d0027520475d7d17b42e06c480ed536d67adbce7
Aresy.z <yzsolo125@gmail.com>
2014-09-26 13:14:16 +0800

    重构slider

--- slider/src/slider.js
+++ slider/src/slider.js
@@ -3,131 +3,36 @@
 	 $.fn.slider = function (options) {
 
 		var settings = {
-			viewNum : 4,
-			scrollNum: 4,
-			content: [
+			viewNum : 2,
+			scrollNum: 2
 
-				{
-					href: 'http://aresyz.com',
-					pic: 'img/group.png',
-					title: '标题一'
-				}, {
-					href: 'http://aresyz.com',
-					pic: 'img/group.png',
-					title: '标题二'
-				}, {
-					href: 'http://aresyz.com',
-					pic: 'img/group.png',
-					title: '标题三'
-				}, {
-					href: 'http://aresyz.com',
-					pic: 'img/group.png',
-					title: '标题四'
-				}
-
-			]
 		};
 
-		var slid = document.getElementById('slider'),              //slider外框
-			items = settings.content,							   
-			items_length = settings.content.length,	               //item数量
-			div_width = (window.outerWidth / settings.viewNum)-10, 
-	    	old_left = slid.style.left,
-	    	old_left_num = old_left.substr(0, old_left.length-2)*1,
-	    	move_steps = (div_width + 10) * settings.scrollNum,
-	    	scrollNum = settings.scrollNum,
-	    	slider_child = document.getElementById('slider').childNodes,	
-	    	slider_ele = [];
+		var img_width = (window.outerWidth / settings.viewNum) - 10,
+			scrollNum = settings.scrollNum,
+			scroll_width = (img_width + 10) * scrollNum,
+			scroll_Width = 0;
 
-		for( var i = 0; i < items_length; i++ ) {
+			console.log(window.outerWidth);
+			console.log(img_width);
 
-			var slid = document.getElementById('slider'), div, img;
-				div = add_Itemstyle();
-				img = add_Imgstyle(i);
-				slid = add_Sliderstyle();
-			    div.appendChild(img);
-		    	slid.appendChild(div);
+		$('.img').css({ width: img_width + 'px' });
 
-		}
-	
-		var move = setInterval(move_left,4000);
-	    
-		get_items();
+		function scroll() {
+			console.log(scroll_width);
 
-	    function get_items() {
-	    	
-	    	for( k in slider_child ) {
-
-	    		if(slider_child[k].nodeType === 1) {
-	    			slider_ele.push(slider_child[k]);
-	    		}
-
-	    	}
-
-	    	var slider_ele_length = slider_ele.length;
-
-	    	for( var i = 0; i<slider_ele_length; i++ ) {
-	    		slider_ele[i].style.left = i * (div_width +10) +'px';
-	    		// console.log('one');
-	    	}
-
-	    }
-
-	    function clone_Node() {
-
-	    	for (var i = 0; i < scrollNum; i++) {
-	    		var div_removed = slider_child[i+1];
-			    var clone_div = div_removed.cloneNode(true);
-			 	clone_div.style.left = ((items_length++) * (div_width +10)) + 'px';
-			    slider.appendChild(clone_div);
-
-			  	// var div_ = slider_child[i+1];
-			  	// div_.style.left = ((items_length++) * (div_width + 10)) + 'px';
-			  	// console.log(div_.style.left);
-			  	// slider.appendChild(div_);
-	    	}
-
-	    	// for( var j = 0; j < scrollNum; j++) {
-	    	// 	console.log('hello');
-	    	// 	delete slider_child[i+1];
-	    	// }
-	    	
-	    }
-
-		function move_left() {	
-
-			old_left_num -= move_steps;
-			$('#slider').animate({left: old_left_num + 'px'}, 400);
-			clone_Node();
+			scroll_Width -= scroll_width;
+			$('.img_box').animate({left: scroll_Width + 'px'}, 400, function () {
+				for (var i = 0; i < scrollNum; i++) {
+					var node = $(this).find('div').eq(i).clone(true);
+					$(this).append(node);
+				}
+			});
 			
-		}
+			// $('.img_box').find('div:lt('+2 + ')').appendTo($('.img_box'));
+			// $('.img_box').css('Left','0px');
 
-		function add_Itemstyle() {
-			var div = document.createElement('li');
-			div.style.width = div_width + 'px';
-			div.style.position = 'absolute';
-			div.style.float = 'left';
-			div.style.height = '140px';
-			div.style.margin = '5px';
-			return div;
 		}
-
-		function add_Imgstyle(i) {
-			var img = document.createElement('img');
-			img.style.width = 100 + '%';
-			img.style.height = 100 + '%';
-			img.src = items[i].pic;
-			return img;
-		}
-
-		function add_Sliderstyle() {
-			slid.style.minWidth = (div_width+20) * items_length + 'px';
-			slid.style.width = 'auto';
-			slid.style.position = 'relative';
-			slid.style.height = '140px';
-			slid.style.left = 0;
-			return slid;
-		}
-
+		var loop = setInterval(scroll, 2000);
 	}
 })(Zepto, window)
