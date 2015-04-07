23324a8a7ef5df75f8da5f03854685922243ecdf
Aresy.z <yzsolo125@gmail.com>
2015-04-07 16:47:57 +0800

    add click

---
+++ router/script/index.js
@@ -0,0 +1,11 @@
+$("#index-span").on('click', function() {
+
+	$(this).find('i').css('left') === '-2px'?
+	side_change('-7px', '0px'):side_change('-2px', '-200px');
+	
+})
+
+function side_change (i, menu) {
+	$("#index-span").find('i').animate({'left': i}, 300);
+	$("#side_menu").animate({'left': menu}, 300, 'ease');
+}
