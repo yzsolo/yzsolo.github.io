bd43a37ca1745f86a5e392e34cf5eee00c71da15
Aresy.z <yzsolo125@gmail.com>
2015-04-07 19:11:28 +0800

    代码重构

--- router/script/index.js
+++ router/script/index.js
@@ -1,37 +1,55 @@
-$("#index-span").on('click', function() {
+var event_bind = {
 
-	$("#index-span").find('i').css('left') === '-3px'?
-	side_change('-9px', '0px', true):side_change('-3px', '-200px', false);
-	
-})
+	eve_describe : {
+		'#index-span click': 'menu_change',
+		'#shade click': 'menu_change'
+	},
 
-$("#shade").on('click', function() {
-	$("#index-span").find('i').css('left') === '-3px'?
-	side_change('-9px', '0px'):side_change('-3px', '-200px');
-})
+	eve_bind : function() {
 
-function side_change (i, menu, flag) {
-	$("#index-span").find('i').animate({'left': i}, 250, 'ease-inout');
-	$("#side_menu").animate({'left': menu}, 250, 'ease-inout');
+		for ( key in this.eve_describe ) {
 
-	if (flag) {
+			if ( this.eve_describe.hasOwnProperty(key) ) {
+				var callback = this.eve_describe[key],
+			    ele_eve = key.split(' '),
+			    ele = ele_eve[0],
+			    eve = ele_eve[1];
+				$(ele).on(eve, this.eve_detail[callback]);
+			}
+		}
+	},
 
-		$("#shade").css({'display': 'block'}).animate({'opacity': '0.7'}, 250);
+	eve_detail : {
 
-	} else {
+		menu_change : function() {
 
-		/*关于链式操作的思考*/
-		//方式1:
-		// $("#shade").animate({'opacity': '0'}, 250).css('display', 'none');
+			$("#index-span").find('i').css('left') === '-3px'?
+			event_bind.side_change('-9px', '0px', true):event_bind.side_change('-3px', '-200px', false);
 
-		//方式2:
-		$("#shade").animate({'opacity': '0'}, 250);
+		}
+	},
 
-		setTimeout(function(){
-			$("#shade").css('display', 'none');
-		},200);
-		
-		
+	side_change : function(i, menu, flag) {
+
+		$("#index-span").find('i').animate({'left': i}, 250, 'ease-inout');
+		$("#side_menu").animate({'left': menu}, 250, 'ease-inout');
+
+		if (flag) {
+			$("#shade").css({'display': 'block'}).animate({'opacity': '0.7'}, 250);
+		} else {
+
+			//方式1:
+			// $("#shade").animate({'opacity': '0'}, 250).css('display', 'none');
+
+			//方式2:
+			$("#shade").animate({'opacity': '0'}, 250);
+
+			setTimeout(function(){
+				$("#shade").css('display', 'none');
+			},200);
+
+		}
 	}
 }
 
+event_bind.eve_bind();
