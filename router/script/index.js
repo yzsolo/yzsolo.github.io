db9117b0ec0339c2370fc0ff78c2708fe47df3f5
Aresy.z <yzsolo125@gmail.com>
2015-04-07 17:01:29 +0800

    change

--- router/script/index.js
+++ router/script/index.js
@@ -1,11 +1,11 @@
 $("#index-span").on('click', function() {
 
 	$(this).find('i').css('left') === '-3px'?
-	side_change('-8px', '0px'):side_change('-3px', '-200px');
+	side_change('-9px', '0px'):side_change('-3px', '-200px');
 	
 })
 
 function side_change (i, menu) {
-	$("#index-span").find('i').animate({'left': i}, 300);
-	$("#side_menu").animate({'left': menu}, 300, 'ease');
+	$("#index-span").find('i').animate({'left': i}, 250, 'ease');
+	$("#side_menu").animate({'left': menu}, 250, 'ease');
 }
